a43a3252ef689081b6a4a56aee9dc198e26960cb
EstebanDBH1 <120125487+EstebanDBH1@users.noreply.github.com>
2023-04-15 20:53:57 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+gsap.from(".social li a", {
+    y: "-100%",
+    ease: "power3.easeInOut",
+    duration: 1,
+    stagger: 0.1,
+    delay: 0.5
+})
+
+gsap.from(".p0 span", {
+    y: "-100%",
+    ease: "power3.easeInOut",
+    duration: 1,
+    delay: 0.6
+})
+
+gsap.from(".p1 span", {
+    y: "-100%",
+    ease: "power3.easeInOut",
+    duration: 1,
+    delay: 0.7
+})
+
+gsap.from(".p3 span", {
+    y: "-100%",
+    ease: "power3.easeInOut",
+    duration: 1,
+    delay: 0.8
+})
+
+gsap.from(".p4 span", {
+    y: "-100%",
+    ease: "power3.easeInOut",
+    duration: 1,
+    delay: 0.9
+})
+
+gsap.from(".p5 span", {
+    y: "-100%",
+    ease: "power3.easeInOut",
+    duration: 1,
+    delay: 1
+})
+
+gsap.from(".pr span", {
+    y: "-100%",
+    ease: "power3.easeInOut",
+    duration: 1,
+    delay: 1.1
+})
+
